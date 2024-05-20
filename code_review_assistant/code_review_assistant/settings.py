5d42d6c48a80b93459148bae5ee53373a1997510
Aaditya Muley <aaditya9@Aadityas-MacBook-Pro.local>
2024-05-20 13:12:25 -0700

    Add CORS handling, Django REST Framework, and API endpoints for code analysis

--- code_review_assistant/code_review_assistant/settings.py
+++ code_review_assistant/code_review_assistant/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "analysis",
+    'analysis',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -126,3 +130,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configure CORS settings to allow your React frontend to access your Django backend
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
